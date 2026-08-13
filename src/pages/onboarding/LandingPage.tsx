f767cf5c9059df7be56317df10d8f832e1a16fed
minJi <kangmj0629@gmail.com>
2026-08-14 02:10:05 +0900

    feat: add landing page UI

--- src/pages/onboarding/LandingPage.tsx
+++ src/pages/onboarding/LandingPage.tsx
@@ -1,11 +1,22 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
-    <main>
-      <h1>Landing</h1>
-      <Link to="/test">Start</Link>
-    </main>
-  )
-}
+    <main className="flex min-h-screen flex-col bg-white">
+      <div className="flex flex-1 items-center justify-center">
+        <h1 className="font-dm-serif text-center text-4xl leading-[1.7]">
+          MCM HOUSE
+          <br />
+          POP-UP
+        </h1>
+      </div>
 
+      <Link
+        to="/home"
+        className="mb-5 self-center px-6 py-5 text-sm font-medium text-[#8A8A86]"
+      >
+        TAP TO ENTER
+      </Link>
+    </main>
+  );
+}
